99197107a6aae51aa3ffab83816fdb44e79c9fd0
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-18 12:30:52 +0530

    Create singly_linked_list.cpp

---
+++ 04.Linked List/singly_linked_list.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+int main() {
+    Node* head = new Node{10, nullptr};
+    head->next = new Node{20, nullptr};
+    head->next->next = new Node{30, nullptr};
+
+    Node* temp = head;
+    while (temp) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    return 0;
+}
