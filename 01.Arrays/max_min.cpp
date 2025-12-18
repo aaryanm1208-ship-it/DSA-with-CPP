c7e8786b48b086265de928e0b4256934f38b052c
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-18 12:28:01 +0530

    Create max_min.cpp

---
+++ 01.Arrays/max_min.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {5, 2, 9, 1, 7};
+    int max = arr[0], min = arr[0];
+
+    for (int i = 1; i < 5; i++) {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
+    }
+
+    cout << "Max = " << max << "\nMin = " << min;
+    return 0;
+}
